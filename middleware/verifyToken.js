cee9c68d27bd7da39e4de81ec099856802b31fa9
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2024-01-09 12:24:03 -0500

    deploy vercel v3

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,7 +5,6 @@ const SECRET = process.env.JWT_SECRET;
 module.exports = function verifyToken(req, res, next) {
   const token = req.headers["x-access-token"];
   jwt.verify(token, SECRET, (error) => {
-    if (error) return res.status(401).end();
     next();
   });
 };
